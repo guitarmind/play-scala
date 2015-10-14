0f693cebcd40b262fe78b0d7717087d2874f87c2
guitarmind <markpeng73@gmail.com>
2015-10-14 16:24:28 +0800

    Practicing...

--- src/main/scala/markpeng/scala/Practice.scala
+++ src/main/scala/markpeng/scala/Practice.scala
@@ -21,7 +21,31 @@ object Practice {
   }
 
   def main(args: Array[String]) {
-    breakable(install)
+    // break simulation
+    //    breakable(install)
+
+    //    val array = new Array[String](3)
+    //    array(0) = "This"
+    //    array(1) = "is"
+    //    array(2) = "markpeng"
+    //    array.foreach(println)
+
+    val oldList = List(1, 2)
+    // add to front
+    val newList = 3 :: oldList
+    println(newList)
+
+    // add to end
+    val newList2 = oldList :+ 3
+    println(newList2)
+
+    // create List through Nil
+    val myList = "This" :: "is" :: "immutable" :: Nil
+    println(myList)
+
+    //    var afterDelete = newList.filterNot { x => x == 3 }
+    val afterDelete = newList.filterNot(_ == 3)
+    println(afterDelete)
   }
 }
 
